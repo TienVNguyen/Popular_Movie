bf08b8a6fe9095ab8fb903dc17453407e99e32b2
TienVNguyen <tien.workinfo@gmail.com>
2019-06-26 09:21:31 -0700

    Initial Project

---
+++ app/src/main/java/com/example/popular_movie/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019. Self Training Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by TIM or Tien Nguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, December 2018
+ */
+
+package com.example.popular_movie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/popular_movie/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019. Self Training Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by TIM or Tien Nguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, December 2018
+ */
+
+package com.example.popular_movie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
